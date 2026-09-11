7e988fa2b13eb1a7f710ce3219ef47ed05a830b7
riyasharma-coder <riyasharma2864@gmail.com>
2026-09-11 20:37:10 +0530

    Time: 228 ms (55.63%), Space: 47 MB (62.76%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -10,22 +10,43 @@ class Pair{
 }
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> Double.compare(a.frac, b.frac));
+
+        PriorityQueue<Pair> pq =
+            new PriorityQueue<>((a,b) -> Double.compare(a.frac, b.frac));
 
         int n = arr.length;
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n; j++){
-                pq.add(new Pair((double) arr[i] / arr[j], arr[i], arr[j]));
-            }
+
+        // Put the smallest fraction for every denominator
+        for(int j=1; j<n; j++){
+            pq.add(new Pair(
+                (double)arr[0] / arr[j],
+                0,
+                j
+            ));
         }
-        
-        for(int i=0; i<k; i++){
+
+        // Get kth smallest
+        for(int count=0; count<k; count++){
+
             Pair curr = pq.poll();
 
-            if(i == k - 1) {
-                return new int[]{curr.i, curr.j};
+            if(count == k-1){
+                return new int[]{arr[curr.i], arr[curr.j]};
+            }
+
+            // Move to next numerator for same denominator
+            if(curr.i + 1 < curr.j){
+                int i = curr.i + 1;
+                int j = curr.j;
+
+                pq.add(new Pair(
+                    (double)arr[i] / arr[j],
+                    i,
+                    j
+                ));
             }
         }
+
         return new int[]{};
     }
 }
